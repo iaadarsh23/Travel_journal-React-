3f340ceff2c505729b1948cc412d024190f802e1
Adarsh <adarshtrip2306@gmail.com>
2025-01-19 23:57:04 +0530

    Acternity setU

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.css";
 import AppHeader from "./components/Header";
+import { AuroraBackground } from "./components/ui/auro-bg";
 
 function App() {
 	return (
 		<>
 			<AppHeader />
+			<AuroraBackground></AuroraBackground>
+			<h1>Hello, Adarsh</h1>
 		</>
 	);
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,43 @@
+const {
+	default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	darkMode: "class",
 	theme: {
-		extend: {},
+		extend: {
+			animation: {
+				aurora: "aurora 60s linear infinite",
+			},
+			keyframes: {
+				aurora: {
+					from: {
+						backgroundPosition: "50% 50%, 50% 50%",
+					},
+					to: {
+						backgroundPosition: "350% 50%, 350% 50%",
+					},
+				},
+			},
+		},
 	},
-	plugins: [],
+	plugins: [addVariablesForColors],
 };
+
+// This plugin adds each Tailwind color as a global CSS variable, e.g., var(--gray-200).
+function addVariablesForColors({ addBase, theme }) {
+	// Flatten the color palette
+	const allColors = flattenColorPalette(theme("colors"));
+
+	// Create CSS custom properties
+	const newVars = Object.fromEntries(
+		Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+	);
+
+	// Add the variables to the :root selector
+	addBase({
+		":root": newVars,
+	});
+}
