c25c6f9765305d21a79cee64737855c8c123a8f3
Adarsh <adarshtrip2306@gmail.com>
2025-01-21 00:29:13 +0530

    props added

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,20 @@ import AppHeader from "./components/Header";
 import { AuroraBackground } from "./components/ui/auro-bg";
 
 function App() {
-	const Time = new Date().getHours();
-	function greeting() {
-		if (Time > 6 && Time < 12) {
-			return "Good morning";
-		} else if (Time > 12 && Time < 15) {
-			return "Good afternoon";
-		} else if (Time > 15 && Time < 18) {
-			return "Good Evening";
-		} else {
-			return "Good Night";
-		}
-	}
 	return (
 		<>
 			<AppHeader />
-			<h1>{greeting()}</h1>
+
 			<AuroraBackground>
-				<EntryPage />
+				<EntryPage
+					image="https://scrimba.com/links/travel-journal-japan-image-url"
+					country="Japan"
+					maplink="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu"
+					place="Mount Fuji"
+					date="12 Jan, 2021 - 24 Jan, 2021"
+					desc={`Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet).
+Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.`}
+				/>
 			</AuroraBackground>
 		</>
 	);
